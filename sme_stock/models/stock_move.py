7a27661d2fdc37b8070c57d70cd0987e94d3204c
Mya Yahmone Hlaing <myayahmonehlaing@gmail.com>
2020-11-11 14:51:22 +0630

    [EDIT] link def _action_done to stock_account

--- sme_stock/models/stock_move.py
+++ sme_stock/models/stock_move.py
@@ -2,12 +2,63 @@ from collections import defaultdict
 from odoo import api, fields, models, _, SUPERUSER_ID
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare, float_round, float_is_zero
+from odoo.addons.stock_account.models.stock_move import StockMove as AccountStockMove
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
+    def _stock_account_before_action_done(self):
+        # Init a dict that will group the moves by valuation type, according to `move._is_valued_type`.
+        valued_moves = {valued_type: self.env['stock.move'] for valued_type in self._get_valued_types()}
+        for move in self:
+            if float_is_zero(move.quantity_done, precision_rounding=move.product_uom.rounding):
+                continue
+            for valued_type in self._get_valued_types():
+                if getattr(move, '_is_%s' % valued_type)():
+                    valued_moves[valued_type] |= move
+                    continue
+
+        # AVCO application
+        valued_moves['in'].product_price_update_before_done()
+        return valued_moves
+
+    def _stock_account_after_action_done(self, res, valued_moves):
+        # '_action_done' might have created an extra move to be valued
+        for move in res - self:
+            for valued_type in self._get_valued_types():
+                if getattr(move, '_is_%s' % valued_type)():
+                    valued_moves[valued_type] |= move
+                    continue
+
+        stock_valuation_layers = self.env['stock.valuation.layer'].sudo()
+        # Create the valuation layers in batch by calling `moves._create_valued_type_svl`.
+        for valued_type in self._get_valued_types():
+            todo_valued_moves = valued_moves[valued_type]
+            if todo_valued_moves:
+                todo_valued_moves._sanity_check_for_valuation()
+                stock_valuation_layers |= getattr(todo_valued_moves, '_create_%s_svl' % valued_type)()
+                continue
+
+        for svl in stock_valuation_layers.with_context(active_test=False):
+            if not svl.product_id.valuation == 'real_time':
+                continue
+            if svl.currency_id.is_zero(svl.value):
+                continue
+            svl.stock_move_id._account_entry_move(svl.quantity, svl.description, svl.id, svl.value)
+
+        stock_valuation_layers._check_company()
+
+        # For every in move, run the vacuum for the linked product.
+        products_to_vacuum = valued_moves['in'].mapped('product_id')
+        company = valued_moves['in'].mapped('company_id') and valued_moves['in'].mapped('company_id')[
+            0] or self.env.company
+        for product_to_vacuum in products_to_vacuum.with_context(active_test=False):
+            product_to_vacuum._run_fifo_vacuum(company)
+
     def _action_done(self, cancel_backorder=False):
+        valued_moves = self._stock_account_before_action_done()
+
         self.filtered(lambda move: move.state == 'draft')._action_confirm()  # MRP allows scrapping draft moves
         moves = self.exists().filtered(lambda x: x.state not in ('done', 'cancel'))
         moves_todo = self.env['stock.move']
@@ -16,7 +67,8 @@ class StockMove(models.Model):
         # this operation could trigger a merge of moves.
         for move in moves:
             if move.quantity_done <= 0:
-                if float_compare(move.product_uom_qty, 0.0, precision_rounding=move.product_uom.rounding) == 0 or cancel_backorder:
+                if float_compare(move.product_uom_qty, 0.0,
+                                 precision_rounding=move.product_uom.rounding) == 0 or cancel_backorder:
                     move._action_cancel()
 
         # Create extra moves where necessary
@@ -34,7 +86,8 @@ class StockMove(models.Model):
             rounding = self.env['decimal.precision'].precision_get('Product Unit of Measure')
             if float_compare(move.quantity_done, move.product_uom_qty, precision_digits=rounding) < 0:
                 # Need to do some kind of conversion here
-                qty_split = move.product_uom._compute_quantity(move.product_uom_qty - move.quantity_done, move.product_id.uom_id, rounding_method='HALF-UP')
+                qty_split = move.product_uom._compute_quantity(move.product_uom_qty - move.quantity_done,
+                                                               move.product_id.uom_id, rounding_method='HALF-UP')
                 new_move = move._split(qty_split)
                 move._unreserve_initial_demand(new_move)
                 if cancel_backorder:
@@ -42,15 +95,19 @@ class StockMove(models.Model):
         moves_todo.mapped('move_line_ids').sorted()._action_done()
         # Check the consistency of the result packages; there should be an unique location across
         # the contained quants.
-        for result_package in moves_todo\
-                .mapped('move_line_ids.result_package_id')\
+        for result_package in moves_todo \
+                .mapped('move_line_ids.result_package_id') \
                 .filtered(lambda p: p.quant_ids and len(p.quant_ids) > 1):
-            if len(result_package.quant_ids.filtered(lambda q: not float_is_zero(abs(q.quantity) + abs(q.reserved_quantity), precision_rounding=q.product_uom_id.rounding)).mapped('location_id')) > 1:
-                raise UserError(_('You cannot move the same package content more than once in the same transfer or split the same package into two location.'))
+            if len(result_package.quant_ids.filtered(
+                    lambda q: not float_is_zero(abs(q.quantity) + abs(q.reserved_quantity),
+                                                precision_rounding=q.product_uom_id.rounding)).mapped(
+                    'location_id')) > 1:
+                raise UserError(_(
+                    'You cannot move the same package content more than once in the same transfer or split the same package into two location.'))
         picking = moves_todo.mapped('picking_id')
 
-        date = self._context.get('force_period_date', fields.Datetime.now()) # myh@smeintellect.com
-        moves_todo.write({'state': 'done', 'date': date}) # myh@smeintellect.com
+        date = self._context.get('force_period_date', fields.Datetime.now())  # myh@smeintellect.com
+        moves_todo.write({'state': 'done', 'date': date})  # myh@smeintellect.com
 
         move_dests_per_company = defaultdict(lambda: self.env['stock.move'])
         for move_dest in moves_todo.move_dest_ids:
@@ -65,8 +122,12 @@ class StockMove(models.Model):
 
         if picking and not cancel_backorder:
             picking._create_backorder()
+
+        self._stock_account_after_action_done(moves_todo, valued_moves)
         return moves_todo
 
+
+AccountStockMove._action_done = StockMove._action_done
 
 
 class StockMoveLine(models.Model):
@@ -99,7 +160,7 @@ class StockMoveLine(models.Model):
                 raise UserError(_('The quantity done for the product "%s" doesn\'t respect the rounding precision \
                                      defined on the unit of measure "%s". Please change the quantity done or the \
                                      rounding precision of your unit of measure.') % (
-                ml.product_id.display_name, ml.product_uom_id.name))
+                    ml.product_id.display_name, ml.product_uom_id.name))
 
             qty_done_float_compared = float_compare(ml.qty_done, 0, precision_rounding=ml.product_uom_id.rounding)
             if qty_done_float_compared > 0:
@@ -188,5 +249,5 @@ class StockMoveLine(models.Model):
 
         (self - ml_to_delete).with_context(bypass_reservation_update=True).write({
             'product_uom_qty': 0.00,
-            'date': date,# myh@smeintellect.com
+            'date': date,  # myh@smeintellect.com
         })
