b3ede12eea3bd6ea2b7c8f9d3f9f9987e2c7a6d5
Mya Yahmone Hlaing <myayahmonehlaing@gmail.com>
2020-11-18 18:34:06 +0630

    [ADD] new module sme_mrp_date

---
+++ sme_mrp_date/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SMEi MRP Date Custom",
+
+    'summary': """
+       Manufacture Date """,
+
+    'description': """
+        Manufacture Date 
+    """,
+
+    'author': "SME Intellect Co. Ltd",
+    'website': "https://www.smeintellect.com/",
+    'category': 'Manufacturing Management',
+    'version': '0.1',
+
+    'depends': ['mrp'],
+    'data': [
+        'views/mrp_production_view.xml',
+    ],
+
+}
