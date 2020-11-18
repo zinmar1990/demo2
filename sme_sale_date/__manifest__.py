4b0af62e43655f6aedd994060c7e1d35ce5aedde
Mya Yahmone Hlaing <myayahmonehlaing@gmail.com>
2020-11-18 09:47:55 +0630

    [ADD] new module sme_sale_date

---
+++ sme_sale_date/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SMEi Sales Custom",
+
+    'summary': """
+        Sales Order Date and Delivery Date""",
+
+    'description': """
+       Sales Order Date and Delivery Date
+    """,
+
+    'author': "SME Intellect Co. Ltd",
+    'website': "https://www.smeintellect.com/",
+    'category': 'Sales Management',
+    'version': '0.1',
+
+    'depends': ['sale_stock'],
+
+    'data': [
+        'views/sale_view.xml'
+    ],
+
+}
